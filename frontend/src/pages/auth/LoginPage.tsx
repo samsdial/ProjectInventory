34b8e40d1358adf605700d6106ab9f7233017bce
Wiliam Sigindioy <wiliamjsm1989@gmail.com>
2024-10-09 17:07:57 -0500

    Login form design, button and input component created

--- frontend/src/pages/auth/LoginPage.tsx
+++ frontend/src/pages/auth/LoginPage.tsx
@@ -3,12 +3,11 @@ import { Button, Input } from "../../components";
 
 export const LoginPage: React.FC = () => {
     return (
-        <div className="">
-            <h1>Login</h1>
-            <form action="">
-                <input type="text" name="" id="" placeholder="Usuario" />
-                <input type="text" name="" id="" placeholder="Contraseña" />
-                <Input type="text" placeholder="User" />
+        <div className="max-w-sm p-6 border rounded-lg text-center shadow-lg">
+            <h1 className="mb-6 text-3xl font-semibold">Login</h1>
+            <form action="" className="flex flex-col gap-3">
+                <Input type="text" placeholder="Usuario" />
+                <Input type="text" placeholder="Contraseña" />
                 <Button color="bg-green-500" text="Iniciar sesión" />
             </form>
         </div>
