6925099e853ae35c996aa4213c7d411acc7bd953
Sergio Andres Martinez E <sergio.martinez@fracttal.com>
2024-10-11 20:54:25 -0500

    full APIRest Products create, delete, update, get

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,10 @@
-import express from 'express';
-import mongoose from "mongoose";
 import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import authRoutes from "./src/routes/authentication";
+import productRoutes from "./src/routes/productRoutes";
 
-import authRoutes from './src/routes/authentication';
-
-dotenv.config(); 
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -13,17 +13,19 @@ console.log("MONGODB_URI:", process.env.MONGODB_URI);
 
 // Connect to MongoDB
 console.log("Attempting to connect to MongoDB...");
-mongoose.connect(process.env.MONGODB_URI as string)
-    .then(() => console.log("Database connected"))
-    .catch(err => {
-        console.error("Could not connect to MongoDB", err);
-        process.exit(1); // Exit the application if the connection fails
-    });
+mongoose
+  .connect(process.env.MONGODB_URI as string)
+  .then(() => console.log("Database connected"))
+  .catch((err) => {
+    console.error("Could not connect to MongoDB", err);
+    process.exit(1); // Exit the application if the connection fails
+  });
 
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
+app.use("/api", productRoutes);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
