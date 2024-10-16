08ce41fb4c9c83c7df8b5c165e23fc25f929020c
Isabella <isabella.serna.ramirez@gmail.com>
2024-10-16 17:48:27 -0500

    Add transactions endpoint

--- backend/src/routes/productRoutes.ts
+++ backend/src/routes/productRoutes.ts
@@ -1,6 +1,5 @@
-// productRoutes.ts
 import express from "express";
-import { createProduct, deleteProduct, getAllProducts, updateProduct } from "../controllers/productController";
+import { createProduct, deleteProduct, getAllProducts, updateProduct, getAllTransactions } from "../controllers/productController";
 
 const router = express.Router();
 
@@ -8,5 +7,6 @@ router.get("/products", getAllProducts);
 router.post("/products", createProduct);
 router.put("/products/:id", updateProduct as express.RequestHandler);
 router.delete("/products/:id", deleteProduct as express.RequestHandler);
+router.get("/transactions", getAllTransactions);
 
 export default router;
